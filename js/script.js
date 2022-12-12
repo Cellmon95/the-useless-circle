9a5dba5655edbefddcf7606316d953fec271e7df
Lucas Källberg <lucas.arne@hotmail.com>
2022-12-12 18:21:55 +0100

    fixed emojis

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
+//global const
 const body = document.querySelector('body');
 const btnClock = document.querySelector('#btn-clock');
-btnClock.addEventListener('click', onClickBtnClock);
 const btnEmojis = document.querySelector('#btn-emojis');
-const btnDots = document.querySelector('#btn-dots');
-document.addEventListener('mousemove', onMouseMove);
-window.onresize = windowResize;
-centerX = window.innerWidth / 2;
-centerY = window.innerHeight / 2;
+
 const circlePoints = [];
 const type = {
   clock: 0,
   emojis: 1,
-  dots: 2,
 };
 
 const emojisList = [
@@ -40,6 +35,18 @@ const emojisList = [
   '☘',
 ];
 
+//event listeners
+btnClock.addEventListener('click', onClickBtnClock);
+btnEmojis.addEventListener('click', onClickBtnEmojis);
+
+document.addEventListener('mousemove', onMouseMove);
+window.onresize = windowResize;
+
+//global variables
+let centerX = window.innerWidth / 2;
+let centerY = window.innerHeight / 2;
+let currentType = type.clock;
+
 randEmojisClock = [];
 
 function initCirclePoints() {
@@ -60,7 +67,7 @@ function onMouseMove(e) {
   const distanceY = e.offsetY - centerY;
   const distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
   const radius = distance - 20;
-  drawCirclePoints(centerX, centerY, radius, type.emojis);
+  drawCirclePoints(centerX, centerY, radius, currentType);
 }
 
 function drawCirclePoints(posX, posY, radius, circleType) {
@@ -78,10 +85,7 @@ function drawCirclePoints(posX, posY, radius, circleType) {
         break;
 
       case type.emojis:
-        clockPoint.innerHTML = emojiPointsType(i);
-        break;
-
-      default:
+        clockPoint.innerHTML = randEmojisClock[i];
         break;
     }
 
@@ -93,7 +97,9 @@ function clockPointsType(i) {
   return ((i + 2) % 12) + 1;
 }
 
-function emojiPointsType(i) {}
+function emojiPointsType(i) {
+  return randEmojisClock[i];
+}
 
 function onClickBtnClock(e) {
   currentType = type.clock;
@@ -102,15 +108,20 @@ function onClickBtnClock(e) {
 function onClickBtnEmojis(e) {
   currentType = type.emojis;
   randEmojisClock = [];
-
-  for (let i = 0; i < 12; i++) {
-    randEmojisClock.push(emojisList[Math.floor(Math.random() * emojis.length)]);
-  }
+  randomiseEmojiList();
 }
 
 function onClickBtnClock(e) {
   currentType = type.clock;
 }
 
+function randomiseEmojiList() {
+  for (let i = 0; i < 12; i++) {
+    randEmojisClock.push(
+      emojisList[Math.floor(Math.random() * emojisList.length)]
+    );
+  }
+}
+
 initCirclePoints();
-drawCirclePoints(centerX, centerY, 150, type.emojis);
+drawCirclePoints(centerX, centerY, 150, currentType);
